156bbc01db1d8561078b28557f12f6892b266ab8
Ivan Petrushev <ivanatora@gmail.com>
2016-05-04 22:50:18 +0300

    Tagging image sizing

--- webroot/js/tag_photos.js
+++ webroot/js/tag_photos.js
@@ -103,19 +103,27 @@ $(document).ready(function(){
     // преоразмеряване на картинката по време на зареждането ѝ
     $("#image_here").load(function(){
         $(this).removeAttr('width');
-        var iWidth = $("#image_here").width();
-        var iMaxWidth = $(window).width() - 100;
-
-        if (iWidth > iMaxWidth){
-            $(this).attr('width', iMaxWidth);
-        }
-
         $(this).removeAttr('height');
-        var iHeight = $("#image_here").height();
-        var iMaxHeight = $(window).height() - 200;
 
-        if (iHeight > iMaxHeight){
-            $(this).attr('height', iMaxHeight)
+        var iWidth = $("#image_here").width();
+        var iHeight = $("#image_here").height();
+        var fRatio = parseFloat(iWidth / iHeight);
+
+        var iMaxWidth = window.innerWidth - 10;
+        var iMaxHeight = window.innerHeight - 300;
+
+        var iDeltaWidth = iWidth - iMaxWidth;
+        var iDeltaHeight = iHeight - iMaxHeight;
+
+        if (iDeltaWidth > iDeltaHeight){
+            $(this).attr('width', iMaxWidth);
+            var fCorrection = iWidth / iMaxWidth;
+            $(this).attr('height', iHeight / fCorrection);
+        }
+        else {
+            $(this).attr('height', iMaxHeight);
+            var fCorrection = iHeight / iMaxHeight;
+            $(this).attr('width', iWidth / fCorrection)
         }
 
     });
